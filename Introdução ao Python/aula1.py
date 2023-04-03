06dc75964b3235fa5573479d05e479e2ce32e71d
Thalisson Vieira <103387093+thsvieira@users.noreply.github.com>
2023-04-03 20:40:48 -0300

    Aula 1 - Função print e Comentários

---
+++ Introdução ao Python/aula1.py
@@ -0,0 +1,18 @@
+'''
+Primeiro estudo em Python
+
+O que aprendi hoje: 
+
+- Finalidade da função print()
+- Usar # para criar comentários em uma linha
+- Usar 3 aspas simples (conforme usado aqui) para criar uma seção de comentários
+
+'''
+
+# Meu primeiro codigo
+
+print('Olá, mundo!')
+print('Tudo bem?')
+print('Que dia é hoje?')
+print('Hoje é sexta-feira!')
+print('Tenha um bom dia!')
