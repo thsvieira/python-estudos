bd44532d06cc1d89bdb77956d4316c0ca16d75f6
Thalisson Vieira <tdevieira@gmail.com>
2023-04-25 18:53:34 -0300

    Aula 13 - Listas

---
+++ Introdução ao Python/aula13.py
@@ -0,0 +1,21 @@
+'''
+Listas - Quando se precisa armazenar alguns dados do mesmo "tipo" como nome de produtos por exemplo, podemos criar algumas
+variaveis e armazenar de forma individual
+
+produto1 = sapato
+produto2 = chinelo
+produto3 = botina
+..e etc.
+
+Porém, isso é pouco pratico. Imagina ter que armazenar 50 nomes de produtos e criar uma variável pra cada um. 
+
+As Listas, servem pra resolver esse problema de forma muito simples, segue um exemplo abaixo:
+'''
+
+calcados = ['Sapato', 'Chinelo', 'Botina', 'Galocha', 'Sandália', 'Coturno'] #Aqui usamos apenas 1 variavel pra armazenar vários itens
+#As listas sempre são identificadas pelas "chaves" [ ], e os itens são registrados entre aspas e separados por vírgulas
+
+print(calcados)#usando a função print + a variavel que armazenou a lista, retorna todos os itens dentro da lista
+
+numeros = ['11', '22', '33', '44', '55'] #Pode ser usado pra armazenar tanto strings quanto numeros
+print(numeros)
